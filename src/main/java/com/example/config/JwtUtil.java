e6f77d5aee06bff8d4dbf9ca7ec55524d59e1a08
p4w10 <p4w10@yandex.ru>
2025-12-09 14:58:57 +0700

    spotbugs

--- src/main/java/com/example/config/JwtUtil.java
+++ src/main/java/com/example/config/JwtUtil.java
@@ -3,6 +3,8 @@ package com.example.config;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import javax.crypto.SecretKey;
 import org.springframework.stereotype.Component;
@@ -21,7 +23,7 @@ public class JwtUtil {
   }
 
   protected SecretKey getKey() {
-    return Keys.hmacShaKeyFor(properties.getSecret().getBytes());
+    return Keys.hmacShaKeyFor(properties.getSecret().getBytes(StandardCharsets.UTF_8));
   }
 
   public String generateToken(String username, int userId) {
