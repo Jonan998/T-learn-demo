c3d0d41bb14841f6a034e7a53bb83fc3371d665b
Selectel Git User <git-user@selectel.ru>
2026-04-01 13:15:15 +0700

    Добавила тренажер + сваггер
    # Conflicts:
    #	pom.xml
    #	src/main/java/ru/teducation/config/SwaggerConfig.java
    #	src/main/java/ru/teducation/service/TrainingServiceImpl.java

--- src/main/java/ru/teducation/config/SwaggerConfig.java
+++ src/main/java/ru/teducation/config/SwaggerConfig.java
@@ -12,21 +12,21 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SwaggerConfig {
 
-  @Bean
-  public OpenAPI customOpenAPI() {
-    final String securitySchemeName = "bearerAuth";
-    return new OpenAPI()
-        .addServersItem(new Server().url("/"))
-        .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
-        .components(
-            new Components()
-                .addSecuritySchemes(
-                    securitySchemeName,
-                    new SecurityScheme()
-                        .name(securitySchemeName)
-                        .type(SecurityScheme.Type.HTTP)
-                        .scheme("bearer")
-                        .bearerFormat("JWT")))
-        .info(new Info().title("T-Education API").version("1.0"));
-  }
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+        return new OpenAPI()
+                .addServersItem(new Server().url("/"))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(
+                        new Components()
+                                .addSecuritySchemes(
+                                        securitySchemeName,
+                                        new SecurityScheme()
+                                                .name(securitySchemeName)
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")))
+                .info(new Info().title("T-Education API").version("1.0"));
+    }
 }
--- src/main/java/ru/teducation/service/TrainingServiceImpl.java
+++ src/main/java/ru/teducation/service/TrainingServiceImpl.java
@@ -13,19 +13,19 @@ import ru.teducation.repository.WordRepository;
 @RequiredArgsConstructor
 public class TrainingServiceImpl implements TrainingService {
 
-  private final WordRepository wordRepository;
+    private final WordRepository wordRepository;
 
-  @Override
-  public AnswerResponseDto checkAnswer(AnswerRequestDto request) {
-    Word word =
-        wordRepository
-            .findById(request.getWordId())
-            .orElseThrow(() -> new NotFoundException("Слово не найдено"));
+    @Override
+    public AnswerResponseDto checkAnswer(AnswerRequestDto request) {
+        Word word =
+                wordRepository
+                        .findById(request.getWordId())
+                        .orElseThrow(() -> new NotFoundException("Слово не найдено"));
 
-    String actualWord = word.getEngLang();
-    String userAnswer = Optional.ofNullable(request.getUserAnswer()).orElse("");
-    boolean isCorrect = actualWord.trim().equalsIgnoreCase(userAnswer.trim());
+        String actualWord = word.getEngLang();
+        String userAnswer = Optional.ofNullable(request.getUserAnswer()).orElse("");
+        boolean isCorrect = actualWord.trim().equalsIgnoreCase(userAnswer.trim());
 
-    return new AnswerResponseDto(isCorrect, actualWord);
-  }
+        return new AnswerResponseDto(isCorrect, actualWord);
+    }
 }
